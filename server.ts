cb7720e988ef9a2dae115c6e6d926ac163a64ff9
Hendrick Menacho <hmenacho97@gmail.com>
2024-02-27 08:58:44 -0300

    initial commit

---
+++ server.ts
@@ -0,0 +1,34 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import { pool } from "./db";
+
+const app = express();
+
+const port = process.env.PORT ?? 5000;
+
+dotenv.config();
+
+app.use(express.json());
+
+app.get("/", async (_req, res) => {
+  const produtos = await pool.query("select * from produtos");
+  return res.json(produtos);
+});
+
+app.post("/", async (req, res) => {
+  const q =
+    "INSERT INTO produtos(nome, categoria, quantidade, valor) VALUES(?)";
+  const values = [
+    req.body.nome,
+    req.body.categoria,
+    req.body.quantidade,
+    req.body.valor,
+  ];
+  await pool.query(q, [values], (err) => {
+    if (err) return res.json(err);
+
+    return res.status(200).json("Produto cadastrado com sucesso.");
+  });
+});
+
+app.listen(port, () => console.log("Server is running on port 5000!"));
