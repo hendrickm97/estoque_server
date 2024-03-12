b3c6600e98beb44f102afc48e690b5476ae48cc8
Hendrick Menacho <hmenacho97@gmail.com>
2024-03-12 20:57:12 -0300

    changing 'valor' to 'valor_unitario'

--- server.ts
+++ server.ts
@@ -24,14 +24,34 @@ app.get("/", async (req, res) => {
 
 app.post("/", async (req, res) => {
   try {
-    const { nome, categoria, quantidade, valor } = req.body;
+    const { nome, categoria, quantidade, valor_unitario } = req.body;
 
     const q =
       "INSERT INTO produtos(nome, categoria, quantidade, valor) VALUES($1, $2, $3, $4) RETURNING *";
 
-    const { rows } = await pool.query(q, [nome, categoria, quantidade, valor]);
+    const { rows } = await pool.query(q, [
+      nome,
+      categoria,
+      quantidade,
+      valor_unitario,
+    ]);
 
     res.json({ data: rows[0] });
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+app.delete("/", async (req, res) => {
+  try {
+    const q = "DELETE FROM produtos where id = $1 RETURNING *";
+
+    const { rows } = await pool.query(q, [req.params]);
+
+    if (rows[0]) {
+      return res.json("Seu produto foi deletado com Sucesso!");
+    }
+    return res.status(404).json("Produto não foi encontrado!");
   } catch (error) {
     res.json(error);
   }
