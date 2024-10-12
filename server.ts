86786712b3a5f66811b10ef5a1886221f513f2c8
Hendrick Menacho <hmenacho97@gmail.com>
2024-10-12 13:47:09 -0300

    feat put path

--- server.ts
+++ server.ts
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", async (req, res) => {
+app.get("/produtos", async (req, res) => {
   try {
     const { rows } = await pool.query("select * from produtos");
     res.json({ data: rows });
@@ -22,7 +22,7 @@ app.get("/", async (req, res) => {
   }
 });
 
-app.get("/:id", async (req, res) => {
+app.get("/produtos/:id", async (req, res) => {
   try {
     const { rows } = await pool.query("SELECT * FROM produtos WHERE id = $1", [
       req.params.id,
@@ -38,7 +38,7 @@ app.get("/:id", async (req, res) => {
     res.status(500).json({ msg: "Erro interno do servidor" });
   }
 }),
-  app.post("/", async (req, res) => {
+  app.post("/produtos", async (req, res) => {
     try {
       const { nome, categoria, quantidade, valorUnitario } = req.body;
 
@@ -59,7 +59,38 @@ app.get("/:id", async (req, res) => {
     }
   });
 
-app.delete("/:id", async (req, res) => {
+app.put("/produtos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { nome, categoria, quantidade, valorUnitario } = req.body;
+
+    const q = `
+        UPDATE produtos 
+        SET nome = $1, categoria = $2, quantidade = $3, "valorUnitario" = $4
+        WHERE id = $5
+        RETURNING *
+      `;
+
+    const { rows } = await pool.query(q, [
+      nome,
+      categoria,
+      quantidade,
+      valorUnitario,
+      id,
+    ]);
+
+    if (rows.length === 0) {
+      return res.status(404).json({ msg: "Produto não encontrado" });
+    }
+
+    res.status(200).json({ data: rows[0] });
+  } catch (error) {
+    console.error("Error in PUT route:", error);
+    res.status(500).json({ msg: "Erro interno do servidor", error });
+  }
+});
+
+app.delete("/produtos/:id", async (req, res) => {
   try {
     const q = "DELETE FROM produtos WHERE id = $1 RETURNING *";
 
